a5fb7776bd1105117eb27576a7109ea85874cc2b
Venkat <kondalueao@gmail.com>
2023-12-16 16:33:44 +0530

    first

--- catalogue.sh
+++ catalogue.sh
@@ -84,6 +84,6 @@ dnf install mongodb-org-shell -y &>> $LOGFILE
 
 VALIDATE $? "Installing MongoDB client"
 
-mongo --host $MONGDB_HOST </app/schema/catalogue.js &>> $LOGFILE
+mongo --host 172.31.36.245 </app/schema/catalogue.js &>> $LOGFILE
 
 VALIDATE $? "Loading catalouge data into MongoDB"
