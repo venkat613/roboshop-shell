8e0716ca97689ff76509d8b96be94710a513341b
Venkat <kondalueao@gmail.com>
2023-12-16 17:38:00 +0530

    first

--- user.sh
+++ user.sh
@@ -25,7 +25,7 @@ then
     echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1 # you can give other than 0
 else
-    echo "You are root user"
+    echo -e " $G You are root user"
 fi # fi means reverse of if, indicating condition end
 
 dnf module disable nodejs -y &>> $LOGFILE
