fb95191d47e6cd1852314917ff08443c430f304a
Venkat <kondalueao@gmail.com>
2023-12-19 20:07:01 +0530

    first

--- roboshop.sh
+++ roboshop.sh
@@ -28,9 +28,10 @@ aws route53 change-resource-record-sets \
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
-              "Value"         : "'$IP_ADDRESS'"
+                "Value"         : "'$IP_ADDRESS'"
             }]
         }
         }]
     }
+        '
 done
