7569c14519bec36acc168a0bb5159eaaf32cdbad
Venkat <kondalueao@gmail.com>
2023-12-19 19:01:17 +0530

    first

--- roboshop.sh
+++ roboshop.sh
@@ -14,4 +14,4 @@ do
     fi
 
  IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
- echo "$i: $IP_ADDRESS"
+echo "$i: $IP_ADDRESS"
